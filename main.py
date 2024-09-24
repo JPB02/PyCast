0a255996d7d2f15557a42d11947fd544035fb7b1
João Barbosa <93934411+JPB02@users.noreply.github.com>
2024-09-24 16:02:21 +0100

    Added Selection of Podcast

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ def search_results(url):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
+        selection = 1 # keep track of choice
 
         # fetch and print podcast results
         for item in data['results']:
-            print(f"Track Name: {item['trackName']}")
+            print(f"Selection: {selection}")
+            print(f"Name: {item['trackName']}")
+            print(f"Artist: {item['artistName']}")
             print("-"*40)
+            selection+=1
 
     else:
         print("API Failure...")
 
+    podcast_choice = input(">> Choose Podcast by ID: ")
+    selected = data['results'][int(podcast_choice)-1]
+    print(selected['trackName'])
+    return selected
 
 def main():
     search = input(">> ").replace(" ", "+")
