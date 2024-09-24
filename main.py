67f3f7be60fc01cf64a0bbe63aa7081629acacdd
João Barbosa <93934411+JPB02@users.noreply.github.com>
2024-09-24 17:26:28 +0100

    Download Episode Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import urllib.request
 import feedparser
 
 # itune's search api
@@ -34,19 +35,61 @@ def parse_RSS(rss):
     parsed_url = feedparser.parse(rss)
     return parsed_url
 
-def get_episode_list(podcast_RSS):
-    for entry in podcast_RSS['entries']:
-        print(entry['title'])
-        
+def get_episode_number(rss):
+    episodeList = rss['entries'] 
+    episode_number = len(episodeList)
+    return episode_number
+
+def choose_episode_from_list(podcast_RSS):
+    episodeList = podcast_RSS['entries'] 
+    episode_number = len(episodeList)
+    for entry in episodeList:
+        print(f"{episode_number}.{entry['title']}")
+        episode_number-=1
+
+    choice = input(">> ID: ")
+    chosen = episodeList[len(episodeList)-int(choice)]['title']
+    print(f"Chosen Episode: {chosen}")
+    return choice
+
+def get_podcast_title(feed):
+    if "title" in feed.feed:
+        return feed.feed.title
+    else:
+        return "No title found"
+
+def download_mp3(feed, episode):
+    episode_number = get_episode_number(feed)
+    podcast_title = get_podcast_title(feed)
+    if not os.path.isdir(podcast_title):
+        os.makedirs(podcast_title)
+
+    entry = feed.entries[episode_number-int(episode)]
+
+    if 'enclosures' in entry:
+        for enclosure in entry.enclosures:
+            if enclosure.type == 'audio/mpeg':
+                mp3_url = enclosure.href
+                episode_title = (entry.title) + ".mp3"
+                file_path = os.path.join(podcast_title, episode_title)
+                    
+                # Download the MP3 and save it to the file path
+                response = requests.get(mp3_url)
+
+                with open(file_path, mode="wb") as file:
+                    file.write(response.content)
+                    print(f"Downloaded: {file_path}, Title:")
 
 def main():
-    search = input(">> ").replace(" ", "+")
+    search = input(">> Search Podcast: ").replace(" ", "+")
     # entity as podcast, this only finds podcasts
     url = api+search+"&entity=podcast"
     podcast = search_results(url)
     RSS = get_RSS(podcast)
+    podcast_title = podcast['trackName']
     parsed_rss = parse_RSS(RSS)
-    get_episode_list(parsed_rss)
+    episode = choose_episode_from_list(parsed_rss)    
+    download_mp3(parsed_rss ,episode)
 
 if __name__ == "__main__":
     main()
